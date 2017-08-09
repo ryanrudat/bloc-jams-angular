2bd1c6e127f2c23f9539d89cdd0d2f31344466a7
Ryan Rudat <ryanrudat@gmail.com>
2017-08-09 11:48:25 -0500

    finished up assignment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,3 @@
-
-
 (function() {
   function config($stateProvider, $locationProvider) {
     $locationProvider
@@ -7,26 +5,26 @@
         enabled: true,
         requireBase: false
       });
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        controller: 'LandingCtrl as landing',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/album',
+        controller: 'AlbumCtrl as album',
+        templateUrl: '/templates/album.html'
+      })
+      .state('collection', {
+        url: '/collection',
+        controller: 'CollectionCtrl as collection',
+        templateUrl: '/templates/collection.html'
+      });
   }
 
     angular
       .module('blocJams', ['ui.router'])
       .config(config);
 
-    });
-
-  $stateProvider
-    .state('landing', {
-      url: '/',
-      controller: 'LandingCtrl as landing',
-      templateUrl: '/templates/landing.html'
-    })
-    .state('album', {
-      url: '/album',
-      templateUrl: '/templates/album.html'
-    })
-    .state('collection', {
-      url: '/collection',
-      controller: 'CollectionCtrl as collection',
-      templateUrl: '/templates/collection.html'
-    });
+})();
