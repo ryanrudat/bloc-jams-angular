49b3ce34ed186b4cdf387f3d312e1ac3426851a3
Ryan Rudat <ryanrudat@gmail.com>
2017-08-15 10:41:08 -0500

    assignment-7 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,12 +2,19 @@
   function SongPlayer() {
     var SongPlayer = {};
 
-    var currentSong = null;
 
-    /**
-    * @desc Buzz object audio file
-    * @type {Object}
-    */
+/**
+* @desc object holds the current song
+* @type {object}
+*/
+
+  var currentSong = null;
+
+
+/**
+* @desc Buzz object audio file
+* @type {Object}
+*/
 
   var currentBuzzObject = null;
 
@@ -31,17 +38,46 @@
 
 };
 
+/**
+* @function playSong
+* @desc plays song from buzz library
+* @param {object} song
+*/
+
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
+    }
+
+/**
+* @function SongPlayer.play
+* @desc method of SongPlayer that checks if a current song is playing, if not, it'll call setSongand playSong function. Also calls if current song is paused.
+* @param {object} song
+*/
+
     SongPlayer.play = function(song) {
       if(currentSong !== song) {
+
         setSong(song);
-        currentBuzzObject.play();
-     song.playing = true;
+        playSong(song):
+
  } else if (currentSong === song) {
      if (currentBuzzObject.isPaused()) {
          currentBuzzObject.play();
      }
    }
 };
+
+/**
+* @function SongPlayer. pause
+* @desc public method of SongPlayer pausing a song
+* @param {object} song
+*/
+
+  SongPlayer.pause = function(song) {
+    currentBuzzObject.pause();
+    song.playing = false;
+  };
 
   return SongPlayer;
   }
